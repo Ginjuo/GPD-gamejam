75c026c8da588cbc41cb862eb4b8f97eb9eec641
gag-xps <201507743@post.au.dk>
2020-08-10 13:14:50 +0200

    Added drunk movement to Ruby

--- game1/Assets/Scripts/RubyController.cs
+++ game1/Assets/Scripts/RubyController.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public int maxHealth = 5;
-    public float speed = 3.0f;
+    public float speed = 3.5f;
     private int _currentHealth;
     public int currentHealth => _currentHealth;
 
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
+
+    // <Drunk movement>
+    public float changeTime = 0.5f; 
+    float timer;                    
+    int drunk_direction = 1;
+    bool drunk_vert;
+    public float drunk_speed_x;
+    public float drunk_speed_y;
+    Vector2 drunk_center;
+    public bool char_move_x;
+    public bool char_move_y;
+    float drunk_acceleration;
+    float drunk_speed_clamp_x;
+    float drunk_speed_clamp_y;
+    public float drunk_pos_clamp;
+    // </Drunk movement>
 
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
@@ -28,6 +44,9 @@ public class RubyController : MonoBehaviour
         animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
         _currentHealth = maxHealth;
+
+        timer = changeTime;                     // Drunk movement
+        drunk_center = rigidbody2d.position;    // Drunk movement
     }
 
     void Launch()
@@ -53,11 +72,17 @@ public class RubyController : MonoBehaviour
 
         Vector2 move = new Vector2(horizontal, vertical);
 
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)) // When someone is controlling
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
+
+            // Drunk movement
+            drunk_center = rigidbody2d.position;
         }
+
+        char_move_x = Mathf.Approximately(move.x, 0.0f) ? false : true;
+        char_move_y = Mathf.Approximately(move.y, 0.0f) ? false : true;
 
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
@@ -69,6 +94,15 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        // Drunk movement
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            drunk_direction = Random.value > 0.5 ? - drunk_direction : drunk_direction;
+            drunk_vert = Random.value > 0.5 ? !drunk_vert : drunk_vert;
+            timer = changeTime;
+        }
     }
 
     void FixedUpdate()
@@ -76,6 +110,8 @@ public class RubyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
+
+        Drunk(ref position);
 
         rigidbody2d.MovePosition(position);
     }
@@ -93,5 +129,47 @@ public class RubyController : MonoBehaviour
         }
         _currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+    }
+    public void Drunk(ref Vector2 position)
+    {
+        drunk_speed_clamp_x = char_move_x ? 1.5f : 0.8f;
+        drunk_speed_clamp_y = char_move_y ? 1.5f : 0.8f;
+
+        Debug.Log("char_move_x" + char_move_x);
+        Debug.Log("char_move_y" + char_move_y);
+
+        if (char_move_x || char_move_y)
+        {
+            drunk_acceleration = 0.2f;
+            drunk_pos_clamp = 10f; // Just large enough to not interfere
+        }
+        else
+        {
+            drunk_acceleration = 0.05f;
+            drunk_speed_clamp_x = 0.2f;
+            drunk_speed_clamp_y = 0.2f;
+            drunk_pos_clamp = 0.2f;
+        }
+        
+        if (drunk_vert)
+        {
+            drunk_speed_y = drunk_speed_y + drunk_direction * drunk_acceleration;
+            drunk_speed_y = Mathf.Clamp(drunk_speed_y, drunk_speed_clamp_y*-1, drunk_speed_clamp_y);
+        }
+        else
+        {
+            drunk_speed_x = drunk_speed_x + drunk_direction * drunk_acceleration;
+            drunk_speed_x = Mathf.Clamp(drunk_speed_x, -drunk_speed_clamp_x, drunk_speed_clamp_x);
+        }
+
+        //Debug.Log("drunk_speed_x: " + drunk_speed_x);
+        //Debug.Log("drunk_speed_y: " + drunk_speed_y);
+        //Debug.Log("drunk_center" + drunk_center);
+
+        float new_pos_y = position.y + Time.deltaTime * drunk_speed_y;
+        float new_pos_x = position.x + Time.deltaTime * drunk_speed_x;
+        position.y = Mathf.Clamp(new_pos_y, drunk_center.y-drunk_pos_clamp, drunk_center.y+drunk_pos_clamp);
+        position.x = Mathf.Clamp(new_pos_x, drunk_center.x-drunk_pos_clamp, drunk_center.x+drunk_pos_clamp);
+
     }
 }
