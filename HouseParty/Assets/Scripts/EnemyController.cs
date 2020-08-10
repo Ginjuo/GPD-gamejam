5fb1a3ee922c0b3f3f1a4757a14289d1cff0a0d5
Frederik <frdrkpm@gmail.com>
2020-08-10 16:04:46 +0200

    Made EnemyController handle drunk

--- HouseParty/Assets/Scripts/EnemyController.cs
+++ HouseParty/Assets/Scripts/EnemyController.cs
@@ -6,7 +6,7 @@ public class EnemyController : MonoBehaviour
 {
     public float speed;
     public bool vertical;
-    public float changeTime = 3.0f;
+    public float changeTime = 0;
     public ParticleSystem smokeEffect;
 
     Rigidbody2D rigidbody2D;
@@ -15,14 +15,29 @@ public class EnemyController : MonoBehaviour
 
     private bool broken = true;
 
-    Animator animator;
+    private Animator animator;
+    // <Drunk movement>
+    int drunk_direction = 1;
+    bool drunk_vert;
+    float drunk_speed_x;
+    float drunk_speed_y;
+    Vector2 drunk_center;
+    public float drunk_pos_clamp; //0.2f
 
+    public float drunk_speed_clamp; //0.1f
+
+    public float drunk_acceleration; //0.2f
+    // </Drunk movement>
+
+    private Vector2 position;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
         animator = GetComponent<Animator>();
+        drunk_center = rigidbody2D.position;
+        drunk_direction = Random.value > 0.5 ? -drunk_direction : drunk_direction;
     }
 
     void Update()
@@ -36,8 +51,8 @@ public class EnemyController : MonoBehaviour
 
         if (timer < 0)
         {
-            direction = Random.value > 0.5 ? - direction : direction;
-            vertical = Random.value > 0.5 ? !vertical : vertical;
+            drunk_direction = Random.value > 0.5 ? -drunk_direction : drunk_direction;
+            drunk_vert = Random.value > 0.5 ? !drunk_vert : drunk_vert;
             timer = changeTime;
         }
     }
@@ -48,19 +63,19 @@ public class EnemyController : MonoBehaviour
         {
             return;
         }
-
         Vector2 position = rigidbody2D.position;
+        Drunk(ref position);
 
-        if (vertical)
+        if (drunk_vert)
         {
-            position.y = position.y + Time.deltaTime * speed * direction;
+            //position.y = position.y + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", 0);
-            animator.SetFloat("Move Y", direction);
+            animator.SetFloat("Move Y", drunk_direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed * direction;
-            animator.SetFloat("Move X", direction);
+            //position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", drunk_direction);
             animator.SetFloat("Move Y", 0);
         }
 
@@ -82,5 +97,17 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    public void Drunk(ref Vector2 position)
+    {
+
+
+        drunk_speed_y = Mathf.Clamp(drunk_speed_y + drunk_direction * drunk_acceleration, -drunk_speed_clamp, drunk_speed_clamp);
+        
+        drunk_speed_x = Mathf.Clamp(drunk_speed_x + drunk_direction * drunk_acceleration, -drunk_speed_clamp, drunk_speed_clamp);
+        
+        position.y = Mathf.Clamp(position.y + Time.deltaTime * drunk_speed_y, drunk_center.y - drunk_pos_clamp, drunk_center.y + drunk_pos_clamp);
+        position.x = Mathf.Clamp(position.x + Time.deltaTime * drunk_speed_x, drunk_center.x - drunk_pos_clamp, drunk_center.x + drunk_pos_clamp);
     }
 }
