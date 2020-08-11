642b23b0638651e5f2f89fa4991d07e00d9cf43c
Johan <lodrian7@mgail.com>
2020-08-11 14:56:09 +0200

    Adding some screens + adding Loader class

---
+++ HouseParty/Assets/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    public enum Scene
+    {
+        EndscreenCar,
+        EndscreenTimer,
+        MainScene,
+        Startscreen
+    }
+
+    public static void Load(Scene sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad.ToString());
+    }
+}
