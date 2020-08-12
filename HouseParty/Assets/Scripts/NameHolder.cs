bdcd1baa423a1148e446e73abdc90f778f109d25
Frederik <frdrkpm@gmail.com>
2020-08-12 09:11:42 +0200

    Added nameholder script for names and infected count. Added timer to game control script

---
+++ HouseParty/Assets/Scripts/NameHolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class NameHolder : MonoBehaviour
+    {
+        public TextMeshProUGUI Name1;
+        public TextMeshProUGUI Name2;
+        public TextMeshProUGUI Name3;
+        public TextMeshProUGUI Name4;
+        public TextMeshProUGUI Name5;
+
+        private readonly List<string> _names = new List<string>( );
+        public string PlayerName => "player";
+
+        public static NameHolder Instance { get; private set; }
+
+        public List<string> InfectedNames { get; set; } = new List<string>();
+
+        public int NumberOfInfected { get; set; }
+
+        void Awake()
+        {
+            DontDestroyOnLoad(this);
+            Instance = this;
+        }
+
+        public List<string> GetNames()
+        {
+            _names.Add("Carl");
+            _names.Add("Bent");
+            _names.Add("Heidi");
+            _names.Add("Bente");
+            return _names;
+        }
+
+    }
+}
